93b0f74dc7027f1702cd6f039ef3b20255918052
Jakub Zielinski <18118271+TheMogg@users.noreply.github.com>
2018-01-09 20:12:34 +0100

    Add Publisher entity and repository

--- src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.example.spring5webapp.bootstrap;
 
 import com.example.spring5webapp.model.Author;
 import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.model.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -14,11 +16,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     @Autowired
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -30,19 +34,23 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData() {
         // Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher harperCollins = new Publisher("Harper Collins", "some address 1");
+        Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        publisherRepository.save(harperCollins);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         // Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Publisher worx = new Publisher("Worx", "some address 2");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", worx);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        publisherRepository.save(worx);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
     }
--- src/main/java/com/example/spring5webapp/model/Author.java
+++ src/main/java/com/example/spring5webapp/model/Author.java
@@ -15,8 +15,8 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
-    public Author(Long id) {
-        this.id = id;
+    public Author() {
+
     }
 
     public Author(String firstName, String lastName) {
